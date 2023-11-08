4983a63423aedd8ff58d86e5813a1c10e33536e4
wiwsic <82441666+wiwsic@users.noreply.github.com>
2023-11-08 18:14:49 -0300

    Version 1 finished!

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-/* 
-
-Pseudo-Code
-
-Welcome the User to the Rock-Paper-Scissor Page/Game;
-Ask if User Want to Play;
-If User hit Cancel prompt "Ok, see you next time!";
-If User hit Ok, The program ask it name;
-If User hit Cancel, the program prompt "Ok, see you next time!";
-If User hit Ok, the program keep the name in a variable;
-Ask user to type "rock","paper" or "scissors", saving the result in userChoice;
-If user type the word wrongly, the Programs ask again;
-Convert the user response to lowCase;
-machineChoice will randomly choice between "rock","paper" or "scissors";
-Program will take the two responses, compare and result the winner;
-
-
-
-*/
-
 
 // Welcome the User to the Rock-Paper-Scissor Page/Game; Ask if User Want to Play;
 
@@ -25,6 +5,7 @@ let wannaPlay = confirm('Welcome to the Rock-Paper-Scissors game!, do you want t
 
 // If User hit Cancel prompt "Ok, see you next time!";
 // If User hit Ok, The program ask it name;
+// If User hit Ok, the program keep the name in a variable;
 
 let playerName;
 
@@ -42,4 +23,97 @@ if (!(wannaPlay)) {
     }
 
 }
+
+
+// Ask user to type "rock","paper" or "scissors", saving the result in userChoice;
+// If user type the word wrongly, the Programs ask again;
+// Convert the user response to lowCase;
+
+let playerChoice = prompt('rock, paper or scissors?','');
+
+playerChoice = playerChoice.toLowerCase();
+
+
+if (!(playerChoice === 'rock' || playerChoice ===  'paper' || playerChoice ===  'scissors')) {
+
+playerChoice = prompt('rock, paper or scissors?','');
+playerChoice = playerChoice.toLowerCase();
+
+} else {
+
+
+
+}
+
+// machineChoice will randomly choice between "rock","paper" or "scissors";
+
+let options = ['rock','paper','scissors'];
+
+let machineChoice = options[Math.floor(Math.random() * 3)];
+
+// Program will take the two responses, compare and result the winner;
+
+function compareResults (a,b) {
+
+    const player = a;
+    const machine = b;
+
+    if (a === 'scissors' && b === 'scissors'){
+
+    return 'Its a draw! You and the machine chose scissors'
+
+    } else if (a === 'paper' && b === 'paper'){
+
+    return 'Its a draw! You and the machine chose paper'
+
+    } else if (a === 'rock' && b === 'rock'){
+
+    return 'Its a draw! You and the machine chose rock'
+
+    } else if ((a === 'scissors' || a === 'paper') && (b === 'scissors' || b === 'paper')){
+
+        if (a === 'scissors') {
+
+            return `Congratulations ${playerName}, you win! You chose scissors and the machine chose paper`
+
+        } else {
+
+            return 'You loose! You chose paper and the machine chose scissors!'
+
+        }
+
+    } else if ((a === 'scissors' || a === 'rock') && (b === 'scissors' || b === 'rock')){
+
+        if (a === 'rock') {
+
+            return `Congratulations ${playerName}, you win! You chose rock and the machine chose scissors`
+
+        } else {
+
+            return 'You loose! You chose scissors and the machine chose rock!'
+
+        }
+
+        
+    } else if ((a === 'paper' || a === 'rock') && (b === 'paper' || b === 'rock')){
+
+        if (a === 'paper') {
+
+            return `Congratulations ${playerName}, you win! You chose paper and the machine chose rock`
+
+        } else {
+
+            return 'You loose! You chose rock and the machine chose paper!'
+
+        }
+
+    }
+
+} 
+
+alert(compareResults(playerChoice,machineChoice));
+
+console.log(playerChoice);
+console.log(machineChoice);
+
 
